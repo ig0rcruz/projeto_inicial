f380c9541ba2d9f0bc36b10609fc1cc8296ee14a
Igor Cruz <igoraugustopl@gmail.com>
2025-03-24 23:33:13 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de Dados")
+
+# variaveis
+idade = 32
+nome = "Igor"
+
+# Print
+print(idade)
+
+#soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Listas invertidas
+lista_invertida = lista.reverse()
